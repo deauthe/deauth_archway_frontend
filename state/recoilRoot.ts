d5ac8628d3c668afd8198b2044af698c5fce01d7
deauthe <deauth@ANURAGs-MacBook-Air.local>
2024-06-19 20:35:17 +0530

    removed useless file

--- state/recoilRoot.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { RecoilRootProps } from "recoil";
-
-export function Root({ children }: { children: React.ReactNode }) {
-	return <RecoilRootProps> {children} </RecoilRootProps>;
-}
